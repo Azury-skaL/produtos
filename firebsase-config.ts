1206cb769ef18d6afb01a83d4fa6dfebe245840d
RAFAEL FELIPE ZAMBELI <rafael.fzambeli@senacsp.edu.br>
2019-03-20 22:08:38 -0300

    banco de dados

---
+++ firebsase-config.ts
@@ -0,0 +1,8 @@
+  export const config = {
+    apiKey: "AIzaSyDsqyI_0P7O4NJ-Fkz0Ds2ru3n_U20O6U8",
+    authDomain: "pedidos-d3c2b.firebaseapp.com",
+    databaseURL: "https://pedidos-d3c2b.firebaseio.com",
+    projectId: "pedidos-d3c2b",
+    storageBucket: "pedidos-d3c2b.appspot.com",
+    messagingSenderId: "949933277545"
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { config } from '../../firebsase-config';
+import { AngularFireModule } from 'angularfire2';
+//import { AngularFireModule } from '@angular/fire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { CategoriasProvider } from '../providers/categorias/categorias';
+import { ProdutosProvider } from '../providers/produtos/produtos';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CategoriasProvider,
+    ProdutosProvider
   ]
 })
 export class AppModule {}
